0d4def390e9568d85173dd053ab17592c086f748
Abu Taleb <abutalebgmtt@gmail.com>
2023-08-17 13:48:29 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/medicalhealthcareapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medicalhealthcareapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:medicalhealthcareapp/screens/welcome_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+     return const MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home : WelcomeScreen(),
+     );
+  }
+
+}
+
+
+
